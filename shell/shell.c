bf02576e1d33fb4e93cad1db07334d1e6524d564
Jie-Gang Kuang <jk3735@columbia.edu>
2014-09-04 13:35:42 -0400

    Add debug build for future tests

--- shell/shell.c
+++ shell/shell.c
@@ -1,3 +1,14 @@
-int main(int argc, char **argv) {
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+#ifdef SHELL_DEBUG
+	int i = 0;
+	printf("argc: %d\n", argc);
+	for (i=0; i<argc; i++)
+		printf("argv[%d]: %s\n", i, argv[i]);
+#else
+	printf("Hello!\n");
+#endif
 	return 0;
 }
