8a701b3a664c8b31113443215aed4fbf106146c8
Jie-Gang Kuang <jk3735@columbia.edu>
2014-09-08 22:23:00 -0400

    Replace previous input handling with getline()

--- shell/shell.c
+++ shell/shell.c
@@ -16,12 +16,8 @@
 #define DBGMSG(...)
 #endif
 
-#define INPUT_BUF_SIZE_UNIT	(1024)
-
-char *input_buf[2] = {NULL, NULL};
-unsigned int input_buf_size = INPUT_BUF_SIZE_UNIT;
-unsigned int input_buf_idx = 0;
-char *input_buf_cur = NULL;
+char *input_buf = NULL;
+unsigned int input_size = 0;
 
 #define INPUT_PIPE_NUM_UNIT	(8)
 
@@ -35,60 +31,6 @@ char *input_token_arr[INPUT_TOKEN_NUM_UNIT];
 inline void print_errno(void)
 {
 	printf("error: %s\n", strerror(errno));
-}
-
-int switch_input_buf(void)
-{
-	int rval = 0;
-	unsigned int next_idx = (input_buf_idx+1) & 0x1;
-	unsigned int next_size = input_buf_size + INPUT_BUF_SIZE_UNIT;
-
-	DBGMSG("Try to switch to input buffer %d, size = %d\n", next_idx, next_size);
-
-	input_buf[next_idx] = malloc(next_size);
-	if (input_buf[next_idx] == NULL) {
-		printf("error: Input buffer switching failed\n");
-		rval = -1;
-	} else {
-		DBGMSG("Input buffer switching succeeded\n");
-		strncpy(input_buf[next_idx], input_buf[input_buf_idx], input_buf_size-1);
-		free(input_buf[input_buf_idx]);
-		input_buf_idx = next_idx;
-		input_buf_size = next_size;
-		input_buf_cur = input_buf[input_buf_idx];
-		DBGMSG("Input swtiched to buffer 0x%X\n", (unsigned int)input_buf_cur);
-	}
-
-	return rval;
-}
-
-int get_input(char **input_buf_p)
-{
-	int rval = 0;
-	unsigned int c_count;
-	char tmp_c;
-
-	c_count = 0;
-	do {
-		tmp_c = getchar();
-		if ((c_count == input_buf_size-1) && tmp_c != '\n') {
-			rval = switch_input_buf();
-			if (rval < 0) {
-				printf("error: Input will be trancated\n");
-				c_count++;
-				break;
-			}
-		}
-		input_buf_cur[c_count] = tmp_c;
-	} while (input_buf_cur[c_count++] != '\n');
-
-	input_buf_cur[c_count-1] = '\0';
-
-	DBGMSG("get_input get input %s\n", input_buf_cur);
-
-	*input_buf_p = input_buf_cur;
-
-	return -1;
 }
 
 int search_input_token(char *input, const char *token, char **output_arr, unsigned int output_arr_num)
@@ -204,7 +146,6 @@ int exec_path(const char *mode, const char *path)
 int main(int argc, char **argv)
 {
 	int rval = 0;
-	char *input = NULL;
 	int pipe_num = 0, token_num = 0;
 	int i = 0;
 
@@ -213,18 +154,6 @@ int main(int argc, char **argv)
 	for (i = 0; i < argc; i++)
 		printf("argv[%d]: %s\n", i, argv[i]);
 #endif
-
-	DBGMSG("Allocate buffer char: %d\n", input_buf_size);
-	DBGMSG("Allocate buffer size: %d\n", input_buf_size * sizeof(char));
-	input_buf[input_buf_idx] = (char *)malloc(input_buf_size * sizeof(char));
-
-	if (input_buf[input_buf_idx] == NULL) {
-		printf("error: Input buffer allocation failed! Shell terminates\n");
-		goto __main_exit;
-	} else {
-		DBGMSG("Input buffer: 0x%X\n", (unsigned int)input_buf[input_buf_idx]);
-		input_buf_cur = input_buf[input_buf_idx];
-	}
 
 	DBGMSG("Allocate pipe number: %d\n", input_pipe_num);
 	DBGMSG("Allocate pipe size: %d\n", input_pipe_num * sizeof(char *));
@@ -238,12 +167,21 @@ int main(int argc, char **argv)
 
 	while (1) {
 		printf("$ ");
-		get_input(&input);
+		rval = getline(&input_buf, &input_size, stdin);
 
-		if (input[0] == '\0')
+		if (rval < 0) {
+			printf("error: Input buffer allocation failed\n");
+			continue;
+		} else {
+			/* From stdin, The last char will be '\n'.
+			   Replace it with '\0'.*/
+			input_buf[rval - 1] = '\0';
+		}
+
+		if (input_buf[0] == '\0')
 			continue;
 
-		pipe_num = search_input_token(input, "|", input_pipe_arr, input_pipe_num);
+		pipe_num = search_input_token(input_buf, "|", input_pipe_arr, input_pipe_num);
 		DBGMSG("Input pipe_num: %d\n", pipe_num);
 
 		for (i = 0; i < pipe_num; i++) {
@@ -289,10 +227,8 @@ int main(int argc, char **argv)
 	}
 
 __main_exit:
-	if (input_buf_cur != NULL) {
-		DBGMSG("Free input buffer: 0x%X\n", (unsigned int)input_buf_cur);
-		free(input_buf_cur);
-	}
+	if (input_buf)
+		free(input_buf);
 
 	if (input_pipe_arr != NULL) {
 		DBGMSG("Free input pipe array: 0x%X\n", (unsigned int)input_pipe_arr);
